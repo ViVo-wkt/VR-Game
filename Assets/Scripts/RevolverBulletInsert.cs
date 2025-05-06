3f7242d237eeb2452be15e54a9f0532dbc237e48
qawith <qawith01@gmail.com>
2025-05-06 14:17:46 +0200

    add revolver script

---
+++ Assets/Scripts/RevolverBulletInsert.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine;
+
+public class BulletChamber : MonoBehaviour
+{
+    public RevolverShoot revolverScript;
+
+    public void OnBulletInserted(SelectEnterEventArgs args)
+    {
+        revolverScript.hasBullet = true;
+        Destroy(args.interactableObject.transform.gameObject); // патрон исчезает при вставке
+    }
+}
---
+++ Assets/Scripts/RevolverShoot.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RevolverShoot : MonoBehaviour
+{
+    [Header("Input")]
+    public InputActionReference triggerAction;
+
+    [Header("Bullet")]
+    public GameObject bulletPrefab;
+    public Transform shootPoint;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip shootClip;
+
+    public bool hasBullet = false;
+
+    private void OnEnable()
+    {
+        triggerAction.action.performed += OnTriggerPressed;
+        triggerAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        triggerAction.action.performed -= OnTriggerPressed;
+        triggerAction.action.Disable();
+    }
+
+    private void OnTriggerPressed(InputAction.CallbackContext context)
+    {
+        if (hasBullet)
+        {
+            Shoot();
+            hasBullet = false;
+        }
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("БАХ!");
+
+        if (bulletPrefab && shootPoint)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                float bulletSpeed = 20f;
+                rb.linearVelocity = shootPoint.forward * bulletSpeed;
+            }
+        }
+
+        if (audioSource && shootClip)
+            audioSource.PlayOneShot(shootClip);
+    }
+
+}
