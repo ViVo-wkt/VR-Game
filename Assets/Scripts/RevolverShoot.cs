f6dad99efb9779775e2d2b7acadf87c4c96c6541
qawith <qawith01@gmail.com>
2025-05-13 00:20:12 +0200

    2 lvl #1

--- Assets/Scripts/RevolverShoot.cs
+++ Assets/Scripts/RevolverShoot.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using static UnityEngine.Rendering.GPUSort;
 
 public class RevolverShoot : MonoBehaviour
@@ -9,15 +11,22 @@ public class RevolverShoot : MonoBehaviour
     public InputActionReference triggerAction;
 
     [Header("Bullet")]
-    public GameObject bulletPrefab;
+    public GameObject LoadbulletPrefab;
+    public GameObject ShootbulletPrefab;
     public Transform shootPoint;
     public Transform spawnPoint;
 
     [Header("Audio")]
     public AudioSource audioSource;
     public AudioClip shootClip;
+    public AudioClip missClip;
+
+    [Header("XR")]
+    public XRSocketInteractor bulletSocket;
 
     public bool hasBullet = false;
+
+    private GameObject loadedBulletObject;
 
     private void OnEnable()
     {
@@ -38,22 +47,33 @@ public class RevolverShoot : MonoBehaviour
             Shoot();
             hasBullet = false;
         }
+        else
+        {
+            audioSource.PlayOneShot(missClip);
+        }
     }
 
     private void Shoot()
     {
         Debug.Log("БАХ!");
 
-        if (bulletPrefab && shootPoint)
+        if (ShootbulletPrefab && shootPoint)
         {
-            GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+            GameObject bullet = Instantiate(ShootbulletPrefab, shootPoint.position, shootPoint.rotation);
 
             Rigidbody rb = bullet.GetComponent<Rigidbody>();
             if (rb != null)
             {
                 float bulletSpeed = 50f;
                 rb.linearVelocity = shootPoint.forward * bulletSpeed;
+                bulletSocket.EndManualInteraction();
 
+                var interactable = bulletSocket.interactablesSelected.FirstOrDefault();
+                if (interactable != null)
+                {
+                    Destroy(loadedBulletObject);
+                    loadedBulletObject = null;
+                }
             }
         }
 
@@ -62,6 +82,11 @@ public class RevolverShoot : MonoBehaviour
     }
     public void SpawnBullet(SelectEnterEventArgs args)
     {
-        Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity);
+        if (loadedBulletObject = null)
+        {
+            loadedBulletObject = args.interactableObject.transform.gameObject;
+            Instantiate(loadedBulletObject, spawnPoint.position, Quaternion.identity);
+        }
+        
     }
 }
